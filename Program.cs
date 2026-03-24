2d5244a3dc24e2ab35a6a22981228565740cb7ef
Odalis Pie <odalisperez_500@hotmail.com>
2026-03-23 20:25:11 -0400

    Refactor Program.cs to streamline initialization

--- Program.cs
+++ Program.cs
@@ -1,16 +1,11 @@
-﻿using FitLife2.Database;
 using FitLife2.Repository;
 using FitLife2.Services;
 using FitLife2.Screens;
 
-// 1. Inicializar la base de datos
-var db = new DatabaseConfig();
-db.Initialize();
-
-// 2. Configurar las capas
-var repo = new MiembroRepository(db);
+// 1. Configurar las capas
+var repo = new MiembroRepository(); 
 var service = new MiembroService(repo);
 var menu = new MenuScreen(service);
 
-// 3. Ejecutar el menú
+// 2. Ejecutar el menú
 menu.Show();
